ddbf00b8b5584259bd1669cafc2047e21659b45e
shaunakk <callmeshaun@users.noreply.github.com>
2025-12-29 19:44:47 +0530

    DJANGO

---
+++ taskflow/core/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializers(serializers.ModelSerializer):
+  class Meta:
+    model=Task
+    fields="__all__"
+    read_only_fields = ["user"]
